d8291c5ec18c01ce4e770c200ef6ef1a01168926
artemwoka <artemwoka@gmail.com>
2025-11-29 14:14:46 +0100

    Add class farm pet.py

--- class farm pet.py
+++ class farm pet.py
@@ -1,4 +1,5 @@
 #Артем Трет'яков
+import random
 class Pet:
     """Віртуальний вихованець."""
 
@@ -8,10 +9,10 @@ class Pet:
     def status():
         print("Загальна кількість звірят:", Pet.total)
    
-    def __init__(self, name, hunger=0, boredom=0):
+    def __init__(self, name):
         self._name = name
-        self.hunger = hunger
-        self.boredom = boredom
+        self.hunger = random.randint(0, 10)
+        self.boredom = random.randint(0, 10)
         Pet.total += 1
     
     @property
@@ -70,9 +71,14 @@ class Pet:
 
 
 def main():
-
-    pet_name = input("Як ви назвете своє звірятко? ")
-    pet = Pet(pet_name)
+    list_farm = []
+    amout = input("Скільки ви хочете мати звіряток?")
+    for i in range(int(amout)):
+        
+        pet_name = input(f"Як ви назвете {i +1} звірятко? ")
+        pet_i = Pet(pet_name)
+        list_farm.append(pet_i)
+    pet_i = None
 
     choice = None
     while choice != "0":
@@ -81,10 +87,10 @@ def main():
             Моє звірятко
 
             0 - Вийти
-            1 - Дізнатися, про самопочуття звірятка
-            2 - Покормити вихованця
-            3 - Пограти з вихованцем
-            4 - Змінити ім'я вихованця
+            1 - Дізнатися, про самопочуття звірят
+            2 - Покормити звірят
+            3 - Пограти з звірятами
+            4 - Змінити ім'я звірят
             """
         )
 
@@ -96,22 +102,30 @@ def main():
             print("До побачення.")
 
         elif choice == "1":
-            pet.talk()
+            for pet in list_farm:
+                pet.talk()
+
 
         elif choice == "2":
-            food = int(input("Скільки їжі дати звірятку? "))
-            pet.eat(food)
+            food = int(input("Скільки їжі дати звірятamu? "))
+            for pet in list_farm:
+                pet.eat(food)
+
 
         elif choice == "3":
-            fun_time = int(input("Скільки часу гратися зі звірятком? "))
-            pet.play(fun_time)
+            fun_time = int(input("Скільки часу гратися зі звірятamu? "))
+            for pet in list_farm:
+                pet.play(fun_time)
 
         elif choice == "4":
             new_name = input("Введіть нове ім'я: ")
-            pet.name = new_name
+            for pet in list_farm:
+                pet.name = new_name
         
         elif choice == "адмін":
-            print(pet)
+            for pet in list_farm:
+
+                print(pet)
 
         else:
             print("Вибачте, у меню немає пункту", choice)
